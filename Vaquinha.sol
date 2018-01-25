7b980a266e2f1eebe30c937b9c7ec10c3362d1e3
de4dL0ck13 <32480743+de4dL0ck13@users.noreply.github.com>
2018-01-25 16:01:48 +1100

    Update Vaquinha.sol

--- Vaquinha.sol
+++ Vaquinha.sol
@@ -4,12 +4,13 @@ contract Vaquinha {
     address manager;
     address icoContract;
     uint quota;                 //fixed amound each member has to send -- future upgrade: make it dynamic
-    uint buyDate;
+    uint endDate;               //deadline of token-sale - ether returned to investors after this date
     uint amountRequired;        //minimum amount reuired to join the ICO
     uint numOfMembers = 0;
+    //future upgrade: manager bonus & bonus to who runs function buyTokens
     
     mapping (uint => address) members;
-    mapping (address => uint) invested;
+    mapping (address => uint) invested;  //quantity of quotas an address has invested
     
     modifier isManager() {
         require(msg.sender == manager);
@@ -17,14 +18,17 @@ contract Vaquinha {
     }
     
     //how many days should the contract wait before buying tokens
-    function Vaquinha (uint _amountRequired, uint _quota, address _icoContract, uint _daysToBuy) public {
-        //set ownership
-        manager = msg.sender;
+    function Vaquinha (uint _amountRequired, 
+                       uint _quota, 
+                       address _icoContract, 
+                       uint _daysToBuy) 
+    public {
+        manager = msg.sender; //set ownership
 
         amountRequired = _amountRequired;
         quota = _quota;
         icoContract = _icoContract;
-        buyDate = now + _daysToBuy * 1 days;
+        endDate = now + _daysToBuy * 1 days;
     }
     
     function getIcoContract() public view returns (address) {
@@ -44,9 +48,63 @@ contract Vaquinha {
             members[numOfMembers] = msg.sender;
         }
         
-        invested[msg.sender] += msg.value;
+        invested[msg.sender]++;
     }
     
     function withdraw() public returns(bool) {
+        uint amount = 0;
+
+        if (invested[msg.sender] > 0) {
+            amount = invested[msg.sender] * quota;
+            assert(this.balance >= amount);
+            
+            invested[msg.sender] = 0;
+            
+            //remove from members list:
+            //find member's ID. If it isn't the last ID, then move last one to 
+            //this position and decrement total number of members
+            for (uint id = 0; id <= numOfMembers; id++) {
+                if (members[id] == msg.sender) {
+                    break;
+                }
+            }
+            
+            if (id == numOfMembers) {
+                members[id] = 0x0;
+            } else {
+                members[id] = members[numOfMembers];
+            }
+            
+            numOfMembers--;
+            
+            msg.sender.transfer(amount);
+            
+            return true;
+        }
+        return false;
+    }
+    
+    function buyTokens() public {
+        
+    }
+    
+    function sendTokens() public {
+        
+    }
+    
+    function refundMembers() private {
+        uint amount;
+        
+        for(uint i; i <= numOfMembers; i++) {
+            amount = invested[members[i]] * quota;
+            if (amount > 0 && this.balance >= amount) {
+                members[i].transfer(amount);
+            }
+        }
+    }
+    
+    function endContract() public isManager {
+        refundMembers();
+        selfdestruct(manager); //bonus?
     }
 }
